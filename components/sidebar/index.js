23bc53579930504f757056ddc0b5a169b99b46bf
Abhinavxox <abhinavxox@gmail.com>
2023-07-30 00:12:54 +0530

    Fix : Changed Sidebar

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styles from "./styles.module.scss";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
@@ -10,7 +11,27 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import { menuArray } from "../../data/home";
+//--------------------
 import { FaBars } from "react-icons/fa";
+import {
+  GiLargeDress,
+  GiClothes,
+  Gi3DHammer,
+  GiWatch,
+  GiBallerinaShoes,
+  GiHeadphones,
+  GiHealthCapsule,
+  GiSportMedal,
+  GiBigDiamondRing,
+} from "react-icons/gi";
+import { MdOutlineSportsEsports, MdOutlineSmartToy } from "react-icons/md";
+import { BiCameraMovie, BiGift, BiCategory } from "react-icons/bi";
+import { FaBaby } from "react-icons/fa";
+import { HiOutlineHome } from "react-icons/hi";
+import { AiOutlineSecurityScan } from "react-icons/ai";
+import { BsPhoneVibrate } from "react-icons/bs";
+import Link from "next/link";
 
 export default function TemporaryDrawer() {
   const [state, setState] = React.useState({
@@ -39,29 +60,62 @@ export default function TemporaryDrawer() {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        <div className={styles.menu}>
+          <ul>
+            <li>
+              <a className={styles.menu__header}>
+                <BiCategory />
+                <b>Categories</b>
+              </a>
+            </li>
+            <div className={styles.menu__list}>
+              {menuArray.map((item, i) => (
+                <li key={i}>
+                  <Link href={item.link}>
+                    {i == 0 ? (
+                      <GiLargeDress />
+                    ) : i == 1 ? (
+                      <GiClothes />
+                    ) : i == 2 ? (
+                      <GiHeadphones />
+                    ) : i == 3 ? (
+                      <GiWatch />
+                    ) : i == 4 ? (
+                      <HiOutlineHome />
+                    ) : i == 5 ? (
+                      <GiHealthCapsule />
+                    ) : i == 6 ? (
+                      <GiBallerinaShoes />
+                    ) : i == 7 ? (
+                      <GiBigDiamondRing />
+                    ) : i == 8 ? (
+                      <GiSportMedal />
+                    ) : i == 9 ? (
+                      <FaBaby />
+                    ) : i == 10 ? (
+                      <BiCameraMovie />
+                    ) : i == 11 ? (
+                      <MdOutlineSportsEsports />
+                    ) : i == 12 ? (
+                      <BsPhoneVibrate />
+                    ) : i == 13 ? (
+                      <MdOutlineSmartToy />
+                    ) : i == 14 ? (
+                      <BiGift />
+                    ) : i == 15 ? (
+                      <Gi3DHammer />
+                    ) : i == 16 ? (
+                      <AiOutlineSecurityScan />
+                    ) : (
+                      ""
+                    )}
+                    <span>{item.name}</span>
+                  </Link>
+                </li>
+              ))}
+            </div>
+          </ul>
+        </div>
       </List>
     </Box>
   );
