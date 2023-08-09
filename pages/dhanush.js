951eb1576fce34f5f4750fe8f9e27c081325608c
Dhanush Kumar G <91822334+dhanu-1104@users.noreply.github.com>
2023-08-09 23:53:12 +0530

    skeleton is ready

---
+++ pages/dhanush.js
@@ -0,0 +1,230 @@
+import styles from "./index.module.css";
+import Image from "next/image";
+import Land from "../public/images/Reactangle.jpeg"
+const ECommerce = () => {
+    return (
+        <div className={styles.eCommerce}>
+            <Image className={styles.eCommerceChild} alt="" src={Land} width={100} height={100} />
+            <img className={styles.eCommerceItem} alt="" src="/rectangle-3@2x.png" />
+            <img className={styles.eCommerceInner} alt="" src="/rectangle-4@2x.png" />
+            <img className={styles.rectangleIcon} alt="" src="/rectangle-6@2x.png" />
+            <img
+                className={styles.eCommerceChild1}
+                alt=""
+                src="/rectangle-12@2x.png"
+            />
+            <img
+                className={styles.eCommerceChild2}
+                alt=""
+                src="/rectangle-5@2x.png"
+            />
+            <img
+                className={styles.eCommerceChild3}
+                alt=""
+                src="/rectangle-8@2x.png"
+            />
+            <img
+                className={styles.eCommerceChild4}
+                alt=""
+                src="/rectangle-10@2x.png"
+            />
+            <img
+                className={styles.eCommerceChild5}
+                alt=""
+                src="/rectangle-11@2x.png"
+            />
+            <img
+                className={styles.eCommerceChild6}
+                alt=""
+                src="/rectangle-13@2x.png"
+            />
+            <img
+                className={styles.eCommerceChild7}
+                alt=""
+                src="/rectangle-17@2x.png"
+            />
+            <div className={styles.rectangleDiv} />
+            <div className={styles.eCommerceChild8} />
+            <div className={styles.small}>
+                <button className={styles.getIt}>
+                    Shop now
+                </button>
+                <img className={styles.icon} alt="" src="/icon1.svg" />
+            </div>
+            <div className={styles.small1}>
+                <b className={styles.getIt}>Kids</b>
+                <img className={styles.icon} alt="" src="/icon2.svg" />
+            </div>
+            <div className={styles.small2}>
+                <b className={styles.getIt}>Explore now</b>
+                <img className={styles.icon} alt="" src="/icon3.svg" />
+            </div>
+            <div className={styles.small3}>
+                <b className={styles.getIt}>Explore now</b>
+                <img className={styles.icon} alt="" src="/icon3.svg" />
+            </div>
+            <div className={styles.small4}>
+                <b className={styles.getIt}>Ladies</b>
+                <img className={styles.icon} alt="" src="/icon2.svg" />
+            </div>
+            <div className={styles.small5}>
+                <b className={styles.getIt}>Men</b>
+                <img className={styles.icon} alt="" src="/icon2.svg" />
+            </div>
+            <b className={styles.aFutureThat}>{`A future that eases your life `}</b>
+            <b className={styles.preferredCategory}>Preferred Category</b>
+            <b className={styles.drapeInStyle}>Drape in style!</b>
+            <div className={styles.flashSale}>{`FLASH SALE ⚡ `}</div>
+            <div className={styles.elevateYourStyle}>
+                Elevate Your Style, Discover the Latest Trends
+            </div>
+            <b className={styles.poplinWrapShirt}>Poplin wrap shirt</b>
+            <b className={styles.b}>$2500</b>
+            <b className={styles.ladies}>Ladies</b>
+            <b className={styles.relaxedFitJumper}>Relaxed fit jumper</b>
+            <b className={styles.b1}>$1500</b>
+            <b className={styles.sweatshirts}>Sweatshirts</b>
+            <b className={styles.b2}>$1000</b>
+            <div className={styles.eCommerceChild9} />
+            <img className={styles.eCommerceChild10} alt="" src="/rectangle-44.svg" />
+            <div className={styles.eCommerceChild11} />
+            <div className={styles.eCommerceChild12} />
+            <b className={styles.b3}>6</b>
+            <b className={styles.b4}>12</b>
+            <b className={styles.b5}>5</b>
+            <b className={styles.b6}>11</b>
+            <b className={styles.days}>Days</b>
+            <b className={styles.hours}>Hours</b>
+            <b className={styles.minutes}>Minutes</b>
+            <b className={styles.seconds}>Seconds</b>
+            <div className={styles.navs}>
+                <div className={styles.logo}>
+                    <img className={styles.logoChild} alt="" src="/star-1.svg" />
+                    <div className={styles.logo1}>Logo</div>
+                </div>
+                <div className={styles.button}>
+                    <div className={styles.signUp}>Sign Up</div>
+                </div>
+                <div className={styles.button1}>
+                    <div className={styles.signUp}>Login</div>
+                </div>
+                <div className={styles.searchBox}>
+                    <img className={styles.icon6} alt="" src="/-.svg" />
+                    <div className={styles.searchForAnythingContainer}>
+                        <span className={styles.search}>{`Search `}</span>
+                        <span className={styles.forAnything}>for anything</span>
+                    </div>
+                </div>
+            </div>
+            <b className={styles.tanks}>Tanks</b>
+            <b className={styles.men}>Men</b>
+            <b className={styles.trousers}>Trousers</b>
+            <b className={styles.ladies1}>Ladies</b>
+            <b className={styles.dresses}>Dresses</b>
+            <b className={styles.kids}>Kid’s</b>
+            <b className={styles.clothing}>Clothing</b>
+            <b className={styles.men1}>Men</b>
+            <b className={styles.tShirts}>T-shirts</b>
+            <div className={styles.freshArrivalsFor}>
+                Fresh arrivals for late summer
+            </div>
+            <b className={styles.tinyTrendsetters}>Tiny Trendsetters</b>
+            <div className={styles.stylishDelightsFor}>Stylish Delights for Kids</div>
+            <div className={styles.beyondTheBasicsContainer}>
+                <p className={styles.beyondTheBasics}>Beyond the Basics</p>
+                <p className={styles.beyondTheBasics}>Elevate Your Workwear Layers</p>
+            </div>
+            <img
+                className={styles.eCommerceChild13}
+                alt=""
+                src="/rectangle-46@2x.png"
+            />
+            <img
+                className={styles.eCommerceChild14}
+                alt=""
+                src="/rectangle-47@2x.png"
+            />
+            <img
+                className={styles.eCommerceChild15}
+                alt=""
+                src="/rectangle-48@2x.png"
+            />
+            <div className={styles.footer6LightMode}>
+                <div className={styles.secondaryFooter}>
+                    <div className={styles.abcgmailcomParent}>
+                        <div className={styles.abcgmailcom}>ABC@gmail.com</div>
+                        <div className={styles.frameChild} />
+                    </div>
+                    <div className={styles.parent}>
+                        <div className={styles.abcgmailcom}>(123) 456-7890</div>
+                        <div className={styles.frameItem} />
+                    </div>
+                </div>
+                <div className={styles.primaryFooter}>
+                    <div className={styles.primaryFooterInner}>
+                        <div className={styles.productParent}>
+                            <div className={styles.product}>
+                                <b className={styles.newsLetter}>Info</b>
+                                <div className={styles.parent}>
+                                    <div className={styles.autocapture}>{`About us `}</div>
+                                    <div className={styles.autocapture}>{`Contact us `}</div>
+                                    <div className={styles.virtualEvents}>
+                                        Social responsibility
+                                    </div>
+                                    <div className={styles.autocapture}>``</div>
+                                </div>
+                            </div>
+                            <div className={styles.product}>
+                                <b className={styles.newsLetter}>{`Help & Support`}</b>
+                                <div className={styles.parent}>
+                                    <div className={styles.autocapture}>Shipping info</div>
+                                    <div className={styles.autocapture}>Returns</div>
+                                    <div className={styles.autocapture}>{`How to order `}</div>
+                                    <div className={styles.autocapture}>How to track</div>
+                                    <div className={styles.autocapture}>Size guide</div>
+                                </div>
+                            </div>
+                            <div className={styles.product}>
+                                <b className={styles.newsLetter}>Customer service</b>
+                                <div className={styles.resourcesParent}>
+                                    <b className={styles.resources}>Customer service</b>
+                                    <div
+                                        className={styles.autocapture}
+                                    >{`Terms & condition`}</div>
+                                    <div className={styles.autocapture}>Transactions</div>
+                                    <div className={styles.autocapture}>Feedback</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.newsletterWrapper}>
+                        <div className={styles.newsletter}>
+                            <div className={styles.newsLetterParent}>
+                                <div className={styles.newsLetter}>News letter</div>
+                                <div className={styles.enterYourEmailAddressParent}>
+                                    <div className={styles.enterYourEmail}>
+                                        Enter your email address
+                                    </div>
+                                    <img
+                                        className={styles.mailIcon}
+                                        alt=""
+                                        src="/mail-icon.svg"
+                                    />
+                                </div>
+                            </div>
+                            <div className={styles.newsletterChild} />
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.abcAllRights}>
+                    © 2022 ABC. All rights reserved.
+                </div>
+                <div className={styles.logo2}>
+                    <b className={styles.newsLetter}>Logo</b>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ECommerce;
